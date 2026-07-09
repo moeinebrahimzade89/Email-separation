2bdab457ede3999b89e5cb1db80ba5f298063435
moeinebrahimzade89 <moein.ebrahimzade.89@gmail.com>
2026-07-09 11:53:11 +0330

    first comit

---
+++ Email separation.py
@@ -0,0 +1,7 @@
+email = input("email:")
+if "@" in email:
+    position = email.find("@")
+    print(f"Username:{email[:position]}")
+    print(f"Domain:{email[position +1 :]}")
+else:
+    print("Your email is incorrect.")
