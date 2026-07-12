9cf1f68d3a305840cc78921bd15f55796dfd2286
Moein Ebrahimzade <moein.ebrahimzade.89@gmail.com>
2026-07-12 21:13:48 +0330

    Update Email separation.py

--- Email separation.py
+++ Email separation.py
@@ -1,8 +1,8 @@
-email = input("email: ")
-
+email = input("email:")
 if "@" in email:
     position = email.find("@")
-    print(f"Username: {email[:position]}")
-    print(f"Domain: {email[position + 1:]}")
+    print(f"Username:{email[:position]}")
+    print(f"Domain:{email[position +1 :]}")
 else:
+    # If the entry does not have an @, it is not an email.
     print("Your email is incorrect.")
