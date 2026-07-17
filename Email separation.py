f2874e44c0aae9d646060f7d4744d740ca12415d
moeinebrahimzade89 <moein.ebrahimzade.89@gmail.com>
2026-07-17 19:37:37 +0330

    Add saving emails to a file

--- Email separation.py
+++ Email separation.py
@@ -2,19 +2,27 @@ from listemail import Domain, English_Name
 
 email_details = []
 
-email = input("email:")
+email = input("email: ")
 if "@" in email:
+    f = open("Save_email.txt", "a")
+    f.write(email + "\n")
+    f.close()
     position = email.find("@")
-    Username = email[:position]
-    Domaine = email[position +1 :]
-    email_type = Domaine
+    username = email[:position]
+    domaine = email[position + 1:]
+    email_type = domaine
     for i in range(len(Domain)):
         if email_type == Domain[i]:
             email_type = English_Name[i]
-    email_details.append({"Username": Username, "Domaine": Domaine, "email_type": email_type})
+    email_details.append(
+        {"username": username, "domaine": domaine, "email_type": email_type}
+    )
     for i, item in enumerate(email_details, start=1):
-        print(f"{i} Username:{item['Username']} Domaine:{item['Domaine']} email_type:{item['email_type']}")
+        print(
+            f"{i} Username:{item['username']} Domaine:{item['domaine']} "
+            f"email_type:{item['email_type']}"
+        )
 else:
     # If the entry does not have an @, it is not an email.
     print("Your email is incorrect.")
-    
+
